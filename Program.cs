4f9243baa85570e28f008b6551b55b09f017b126
Asaf <asaf9398@gmail.com>
2023-04-28 16:18:23 +0300

    fixing port numbr to 8946

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace ThirdAssignment_Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:8496");
                 });
     }
 }
