c262fa38f3ebeb67a015f9b2f786c201d84d3e41
Asaf <asaf9398@gmail.com>
2023-05-27 15:56:16 +0300

    deleting unnecessary code and creating shell for LoggerController class

---
+++ Controllers/LoggerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Core;
+using System.Reflection;
+
+namespace ThirdAssignment_Server.Controllers
+{
+    public class LoggerController : Controller
+    {
+        [HttpGet]
+        [Route("/logs/level")]
+        public ActionResult<string> GetLevel([FromQuery(Name = "logger-name")] string loggerName)
+        {
+            if (loggerName != "request-logger" && loggerName != "todo-logger")
+            {
+                return StatusCode(400, "No such logger");
+            }
+            ILog logger = LogManager.GetLogger(Assembly.GetExecutingAssembly(),loggerName);
+            //Level loggerLevel = ((ILogger)logger.Logger).Level;
+            //Level loggerLevel=(logger.Logger).Repository.
+            //log4net.Config.ConfiguratorAttribute
+            return StatusCode(200, "No such logger");
+        }
+    }
+}
--- Controllers/ToDoListController.cs
+++ Controllers/ToDoListController.cs
@@ -72,8 +72,7 @@ namespace ThirdAssignment_Server.Controllers
             stopwatch.Start();
             int requestNumber = RequestsCounter.GetCounter();
             log4net.GlobalContext.Properties["request-number"] = requestNumber;
-            _requestsLogger.Info($"Incoming request | #{requestNumber} | resource: {HttpContext.Request.Path} | HTTP Verb {HttpContext.Request.Method}");
-            string errorMessage="";
+            _requestsLogger.Info($"Incoming request | #{requestNumber} | resource: {HttpContext.Request.Path} | HTTP Verb {HttpContext.Request.Method}");            
 
             int numOfToDo = countToDoByFilter(status);
             if (numOfToDo < 0)
