333e942f70bc596fa7ab91fa6a7d21d88d317fa7
Asaf <asaf9398@gmail.com>
2023-05-27 11:46:42 +0300

    adding printing log to the screen + RequestsCounter class

--- Controllers/Health.cs
+++ Controllers/Health.cs
@@ -4,16 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using log4net;
 
 namespace ThirdAssignment_Server.Controllers
 {
     public class Health : Controller
-    { 
+    {
+        private ILog _requestsLogger;
+        private ILog _toDoLogger;
+
+        public Health()
+        {
+            _requestsLogger = LogManager.GetLogger("request-logger");
+            _toDoLogger = LogManager.GetLogger("todo-logger");
+        }
+
         // Get: Health
         [HttpGet]
         [Route("/todo/health")]
         public ActionResult<string> CheckHealth()
         {
+            _requestsLogger.Info("testing requests logger");
+            _toDoLogger.Info("testing todo logger");
             string message = "OK";
             return Ok(message);
         }
---
+++ RequestsCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThirdAssignment_Server
+{
+    public static class RequestsCounter
+    {
+        private static int counter = 1;
+        public static int GetCounter()
+        {
+            int oldCounter = counter;
+            counter++;
+            return oldCounter;
+        }
+    }
+}
