4c65e27bca970c1742e9e98cf57873a5bbabf3e6
Asaf <asaf9398@gmail.com>
2023-05-27 18:02:39 +0300

    adding logging to the LoggerController

--- Controllers/LoggerController.cs
+++ Controllers/LoggerController.cs
@@ -9,18 +9,35 @@ using log4net.Core;
 using System.Reflection;
 using log4net.Repository;
 using log4net.Repository.Hierarchy;
+using System.Diagnostics;
 
 namespace ThirdAssignment_Server.Controllers
 {
     public class LoggerController : Controller
     {
+        private ILog _requestsLogger;
+        public LoggerController()
+        {
+            _requestsLogger = LogManager.GetLogger("request-logger");         
+        }
+
         [HttpGet]
         [Route("/logs/level")]
         public ActionResult<string> GetLevel([FromQuery(Name = "logger-name")] string loggerName)
         {
+            //logging
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int requestNumber = RequestsCounter.GetCounter();
+            log4net.GlobalContext.Properties["request-number"] = requestNumber;
+            _requestsLogger.Info($"Incoming request | #{requestNumber} | resource: {HttpContext.Request.Path} | HTTP Verb {HttpContext.Request.Method}");
+            string errorMessage;
+
             if (loggerName != "request-logger" && loggerName != "todo-logger")
             {
-                return StatusCode(400, "No such logger");
+                errorMessage = $"No such logger";
+                StopWatchAndWriteLog(stopwatch, requestNumber);
+                return StatusCode(400, errorMessage);
             }
             ILog logger = LogManager.GetLogger(loggerName);
             ILoggerRepository repository = LogManager.GetRepository();
@@ -34,8 +51,15 @@ namespace ThirdAssignment_Server.Controllers
                 // Access the logger level properties
                 levelName = loggerLevel.Name; // e.g., "INFO", "DEBUG", etc.
                 levelValue = loggerLevel.Value; // e.g., 20000, 10000, etc.
-                                                   // ...
+                                                // ...
             }
+            else
+            {
+                errorMessage = $"Error while getting level from {loggerName}";
+                return StatusCode(409, errorMessage);
+            }
+
+            StopWatchAndWriteLog(stopwatch, requestNumber);
             return StatusCode(200, $"{levelName}");
         }
 
@@ -44,26 +68,56 @@ namespace ThirdAssignment_Server.Controllers
         [Route("/logs/level")]
         public ActionResult<string> SetLevel([FromQuery(Name = "logger-name")] string loggerName, [FromQuery(Name = "logger-level")] string newLoggerLevel)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int requestNumber = RequestsCounter.GetCounter();
+            log4net.GlobalContext.Properties["request-number"] = requestNumber;
+            _requestsLogger.Info($"Incoming request | #{requestNumber} | resource: {HttpContext.Request.Path} | HTTP Verb {HttpContext.Request.Method}");
+            string errorMessage;
+
             if (loggerName != "request-logger" && loggerName != "todo-logger")
             {
-                return StatusCode(400, "No such logger");
+                errorMessage = $"No such logger named {loggerName}";               
+                StopWatchAndWriteLog(stopwatch, requestNumber);
+                return StatusCode(400, errorMessage);
             }
+
             if (newLoggerLevel != "ERROR" && newLoggerLevel != "DEBUG" && newLoggerLevel != "INFO")
             {
-                return StatusCode(400, "No such level");
+                errorMessage = $"No such level \"{newLoggerLevel}\"";              
+                StopWatchAndWriteLog(stopwatch, requestNumber);
+                return StatusCode(400, errorMessage);
             }
+
             ILog logger = LogManager.GetLogger(loggerName);
             ILoggerRepository repository = LogManager.GetRepository();
             Logger loggerObject = (Logger)repository.GetLogger(logger.Logger.Name);
+            if (loggerObject == null)
+            {
+                errorMessage = $"Error while getting {loggerName} logger object";                
+                StopWatchAndWriteLog(stopwatch, requestNumber);
+                return StatusCode(409, errorMessage);
+            }
 
             // Get the log level from the LevelMap
             Level level = loggerObject.Hierarchy.LevelMap[newLoggerLevel];
-
+           
+            if (level==null)
+            {
+                errorMessage = $"Error while getting {loggerName} level property";               
+                StopWatchAndWriteLog(stopwatch, requestNumber);
+                return StatusCode(409, errorMessage);
+            }
             // Set the log level for the logger
-            loggerObject.Level = level;
-
-
+            loggerObject.Level = level;                    
+            StopWatchAndWriteLog(stopwatch, requestNumber);
             return StatusCode(200, $"{newLoggerLevel}");
+        }
+        void StopWatchAndWriteLog(Stopwatch stopwatch, int requestNumber)
+        {
+            stopwatch.Stop();
+            long elapsedTimeMs = stopwatch.ElapsedMilliseconds;
+            _requestsLogger.Debug($"request #{requestNumber} duration: {elapsedTimeMs}ms");
         }
     }
 }
